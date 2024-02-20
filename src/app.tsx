26950347f1ac9590cdd039003311170ea2508acd
Erica de Bem <ericadebemm@gmail.com>
2024-02-20 19:45:00 +0100

    Add Lucide-React icons and table component

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,16 @@
-import { Plus, Search } from "lucide-react";
+import { Plus, Search, FileDown, MoreHorizontal } from "lucide-react";
 import { Header } from "./components/header";
 import { Tabs } from "./components/tabs";
 import { Button } from "./components/ui/button";
 import { Control, Input } from "./components/ui/input";
-
-
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "./components/ui/table";
 
 export function App() {
   return (
@@ -25,9 +31,44 @@ export function App() {
         <div className="flex items-center justify-between">
           <Input variant="filter">
             <Search className="size-3" />
-            <Control placeholder="Search tags..."/>
+            <Control placeholder="Search tags..." />
           </Input>
+
+          <Button>
+            <FileDown className="size-3" />
+            Export
+          </Button>
         </div>
+
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead></TableHead>
+              <TableHead>Tag</TableHead>
+              <TableHead>Amount of videos</TableHead>
+              <TableHead></TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell></TableCell>
+              <TableCell>
+                <div className="flex flex-col gap-0.5">
+                  <span className="font-medium">React</span>
+                  <span className="text-xs text-zinc-500">
+                    9E0E95BF-4BF6-4A23-B79E-F71AE9C9C117
+                  </span>
+                </div>
+              </TableCell>
+              <TableCell className="text-zinc-300">13 Video (s)</TableCell>
+              <TableCell className="text-right">
+                <Button size="icon">
+                  <MoreHorizontal className="size-4" />
+                </Button>
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
       </main>
     </div>
   );
